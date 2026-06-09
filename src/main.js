9877adc983b3caf2c40402a341d22db61511969a
teohes <teohes@users.noreply.github.com>
2026-06-09 18:06:07 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './style.css'
+import javascriptLogo from './assets/javascript.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello from Inflexion Partners</h1>
+
+  <p>Deployed from GitHub to Vercel.</p>
+`
